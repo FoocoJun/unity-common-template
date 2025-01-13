0caf1ce032251e6206deaa744d1ce85dcbcce7ac
Dev_Jun <songhajun648@gmail.com>
2025-01-13 17:26:21 +0900

    create some TextData for examples

---
+++ Assets/@Scripts/Data/Data.Contents.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Data
+{
+    #region TextData
+    [Serializable]
+    public class TextData
+    {
+        public string DataId;
+        public string KOR;
+    }
+
+    [Serializable]
+    public class TextDataLoader : ILoader<string, TextData>
+    {
+        public List<TextData> texts = new List<TextData>();
+        public Dictionary<string, TextData> MakeDict()
+        {
+            Dictionary<string, TextData> dict = new Dictionary<string, TextData>();
+            foreach (TextData text in texts)
+                dict.Add(text.DataId, text);
+            return dict;
+        }
+    }
+    #endregion
+}
